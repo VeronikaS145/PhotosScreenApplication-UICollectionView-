9ca7311928e8fbdbcc880b76fd0da333247b2fef
VeronikaSemenkova <nika.siemienkova145@gmail.com>
2023-02-19 16:26:18 +0300

    Add collectionView.

--- PhotosScreenApplication(UICollectionView)/Sources/MainModule/ViewController.swift
+++ PhotosScreenApplication(UICollectionView)/Sources/MainModule/ViewController.swift
@@ -6,23 +6,69 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     // MARK: - Outlets
     
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        setupView()
+        setupHierarchy()
+        setupLayout()
+        createLayout()
     }
 
     // MARK: - Setups
+    private func setupView() {
+        view.backgroundColor = .yellow
+        title = "Albums"
+    }
     
+    private func setupHierarchy() {
+        view.addSubview(collectionView)
+    }
+    
+    private func setupLayout() {
+        collectionView.snp.makeConstraints { make in
+            make.left.top.right.bottom.equalTo(view)
+        }
+    }
     
     // MARK: - Actions
 
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (section, _) -> NSCollectionLayoutSection in
+            
+        }
+    }
+}
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
 }
 
